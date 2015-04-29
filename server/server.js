e9df9bc5160592b8522df93c71fb6fdbe3242516
Phil Dornfeld <phillydorn75@gmail.com>
2015-04-29 15:04:01 -0500

    Adds mongo db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require ('body-parser');
 var app = express();
+var mongo = require('mongodb');
+var monk =require ('monk');
+var db = monk('mongodb://heroku_app36344810:slkuae58qandst6sk9r58r57bl@ds031812.mongolab.com:31812/heroku_app36344810');
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('./client'));
