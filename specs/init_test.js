b0f133b796879e1351abd4b0ea458a76dbb4ed05
Pat Dalberg <pat.dalberg@gmail.com>
2015-04-29 10:13:05 -0500

    initial test for Travis CI integration

---
+++ specs/init_test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('CodeColab minimal test to initialize Travis connectivity',function(){
+
+  describe('BOGOTEST - REPLACE WITH REAL TESTS',function(){
+    
+    it('TRUE = TRUE',function(){
+      expect(true).toBe(true);
+    });
+
+  });
+
+});
