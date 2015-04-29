f5265015e54117824e3afa7ffbc0236996dacbf6
Phil Dornfeld <phillydorn75@gmail.com>
2015-04-28 19:12:48 -0500

    basic server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + './client/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
