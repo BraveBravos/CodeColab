8f5cb621736e04205ace21596f8c03d310fc5045
Enja Reyes <enja.reyes@gmail.com>
2015-04-30 10:01:33 -0500

    completed basic oauth flow

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ var monk =require ('monk');
 var docs = require('./documents/documents.js');
 var db = monk('mongodb://heroku_app36344810:slkuae58qandst6sk9r58r57bl@ds031812.mongolab.com:31812/heroku_app36344810');
 
+var session = require('express-session'),
+    path = require('path'),
+    passport = require('passport'),
+    GitHubStrategy = require('passport-github').Strategy;
+
+
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('./client'));
 app.use(bodyParser.json());
@@ -23,5 +29,39 @@ app.post('/api/documents', function (req, res){
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+passport.use(new GitHubStrategy({
+    // clientID: process.env.GITHUB_CLIENT_ID,
+    // clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    clientID: 'b127ac98c63ddde943a4',
+    clientSecret: '3d1734cea8816504187c53db26ef8530bab85c7f',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log('inside gitHubStrategy')
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+    //create user table
+    //store githubID in DB
+  }
+));
+
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('inside redirect for /auth/github/callback')
+    // Successful authentication, redirect home.
+    res.redirect('/main');
+  });
+
+
+
+
+
 
 
