f642d478b1728ce795b868859b10366744c4f162
Adam <adam.l.guerra@gmail.com>
2015-05-06 15:03:25 -0500

    working in a kind of hack-y way.

--- client/app/services.js
+++ client/app/services.js
@@ -47,67 +47,59 @@ var loadShare = function ($scope) {
     console.log('socket',socket)
     var sjs = new sharejs.Connection(socket);
     var doc = sjs.get('documents', repo);
-    // var doc = sjs.get('documents',undefined,repo,function(error,doc){
-    //   console.log('opened',doc)
-    //   doc.subscribe()
-    //   doc.attachCodeMirror(codeEditor.editor())
-    //   codeEditor.editor().setValue(doc.snapshot || 'test')
-    // })
+
     console.log('doc',doc)
     doc.subscribe()
-    // var codeEditor = CodeMirror.MergeView(document.getElementById('area'), {
-    //   'origRight':'', //this contains the original code
-    //   'value':'',      //this will be the updated value with the users' changes
-    //   'theme':'erlang-dark',
-    //   lineNumbers: true
-    // })
+
+    //this is the element we have selected to "turn" into a CodeMirror
+    var target = document.getElementById('area') 
+    
+    //when we change an element to a CodeMirror, we don't really change it - we actually append
+    //the CodeMirror.  This code deletes any children that already exist.
+    var children = target.getElementsByClassName('CodeMirror-merge')
+    if(!!children.length) {
+      target.removeChild(children[0]) //there should only ever be one of these
+    }
     doc.whenReady(function() {
-      if (!doc.type) {console.log('created');doc.create('text')}
+      if (!doc.type) {
+        console.log('created');
+        doc.create('text');
+      }
 
-      var codeEditor = CodeMirror.MergeView(document.getElementById('area'), {
-      'origRight':'', //this contains the original code
-      'value':doc.getSnapshot(),      //this will be the updated value with the users' changes
-      'theme':'erlang-dark',
-      lineNumbers: true
+      var codeEditor = CodeMirror.MergeView(target, {
+        'origRight':'', //this will eventually contain the original code
+        'value':doc.getSnapshot(),      //this is the updated value with the users' changes
+        'theme':'erlang-dark',
+        lineNumbers: true
       })
-      console.log(codeEditor)
-
-
-      // else {console.log('found',doc.snapshot)}
-      // this check is probably not necessary
-      // if (doc.type && doc.type.name === 'text')
-      // this updates the CodeMirror editor window
+      
       console.log('ready')
       doc.subscribe(function(err) {
         console.log('subscribed',doc.getSnapshot())
-        codeEditor.editor().setValue(doc.getSnapshot() || 'test')
-        // codeEditor.editor().save()
+
+        // instead of deleting and re-adding editors, it might be possible
+        // to do a swapDoc from CodeMirror.
         doc.attachCodeMirror(codeEditor.editor())
         console.log('after subscribed',doc.getSnapshot(),codeEditor.editor().getValue())
         
       });
+      
+      codeEditor.editor().on('change', function(change) {
+        console.log('changed',change)
+      })
+      codeEditor.editor().on('update', function() {
+        console.log('updated')
+      })
     });
-    //   doc.fetch(function(err) {
-    //     console.log('fetched')
-    //   })
-    // })
-    // codeEditor.editor().setValue(doc.snapshot)
 
-      // doc.fetch(function(err) {
-      //   console.log('fetched')
-      //   doc.subscribe(function(err) {
-      //   doc.attachCodeMirror(codeEditor.editor())
-      //   codeEditor.editor().setValue(doc.snapshot || 'test')
-      //   })
-      // })
     //need to finish importing all of the sublime shortcuts and whatnot: http://codemirror.net/doc/manual.html#addons
 
     // this is the syntax needed for .getValue and .setValue.  rightOriginal, leftOriginal, and editor are all
     // of the possible CodeMirror instances; we only use editor and rightOriginal in our version right now.
     // console.log('editor: ',codeEditor.editor().getValue(),"\n",'original: ',codeEditor.rightOriginal().getValue())
     // codeEditor.editor().setValue('this is a test')
-    return {doc:doc,socket:socket}
-    // return codeEditor
+
+    return codeEditor
   }
 
 
