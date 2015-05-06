31ad14d61b711ddc757a88595e4a88ddcba8cd27
Adam <adam.l.guerra@gmail.com>
2015-05-06 15:25:35 -0500

    formatting, comments

--- client/app/services.js
+++ client/app/services.js
@@ -33,74 +33,67 @@ var getRepos = function ($scope) {
 
 
 var loadShare = function ($scope) {
-  if(doc) {
-    console.log('found doc')
-    doc.unsubscribe()
+  var repo = $scope.selected;
+  // console.log('repp', repo)
+
+  var socket = new BCSocket(null, {reconnect: true});
+  // console.log('socket',socket)
+  var sjs = new sharejs.Connection(socket);
+  var doc = sjs.get('documents', repo);
+
+  // console.log('doc',doc)
+  doc.subscribe()
+
+  //this is the element we have selected to "turn" into a CodeMirror
+  var target = document.getElementById('area') 
+  
+  //when we change an element to a CodeMirror, we don't really change it - we actually append
+  //the CodeMirror.  This code deletes any children that already exist.
+  var children = target.getElementsByClassName('CodeMirror-merge')
+  if(!!children.length) {
+    target.removeChild(children[0]) //there should only ever be one of these
   }
-  if(socket) {
-    console.log('socket here',socket)
-  }
-    var repo = $scope.selected;
-    console.log('repp', repo)
-
-    var socket = new BCSocket(null, {reconnect: true});
-    console.log('socket',socket)
-    var sjs = new sharejs.Connection(socket);
-    var doc = sjs.get('documents', repo);
-
-    console.log('doc',doc)
-    doc.subscribe()
-
-    //this is the element we have selected to "turn" into a CodeMirror
-    var target = document.getElementById('area') 
-    
-    //when we change an element to a CodeMirror, we don't really change it - we actually append
-    //the CodeMirror.  This code deletes any children that already exist.
-    var children = target.getElementsByClassName('CodeMirror-merge')
-    if(!!children.length) {
-      target.removeChild(children[0]) //there should only ever be one of these
+  doc.whenReady(function() {
+    if (!doc.type) {
+      console.log('created');
+      doc.create('text');
     }
-    doc.whenReady(function() {
-      if (!doc.type) {
-        console.log('created');
-        doc.create('text');
-      }
 
-      var codeEditor = CodeMirror.MergeView(target, {
-        'origRight':'', //this will eventually contain the original code
-        'value':doc.getSnapshot(),      //this is the updated value with the users' changes
-        'theme':'erlang-dark',
-        lineNumbers: true
-      })
-      
-      console.log('ready')
-      doc.subscribe(function(err) {
-        console.log('subscribed',doc.getSnapshot())
+    var codeEditor = CodeMirror.MergeView(target, {
+      'origRight':'', //this will eventually contain the original code
+      'value':doc.getSnapshot(),      //this is the updated value with the users' changes
+      'theme':'erlang-dark',
+      lineNumbers: true
+    })
+    
+    console.log('ready')
+    doc.subscribe(function(err) {
+      console.log('subscribed',doc.getSnapshot())
 
-        // instead of deleting and re-adding editors, it might be possible
-        // to do a swapDoc from CodeMirror.
-        doc.attachCodeMirror(codeEditor.editor())
-        console.log('after subscribed',doc.getSnapshot(),codeEditor.editor().getValue())
-        
-      });
+      // instead of deleting and re-adding editors, it might be possible
+      // to do a swapDoc from CodeMirror.
+      doc.attachCodeMirror(codeEditor.editor())
+      console.log('after subscribed',doc.getSnapshot(),codeEditor.editor().getValue())
       
-      codeEditor.editor().on('change', function(change) {
-        console.log('changed',change)
-      })
-      codeEditor.editor().on('update', function() {
-        console.log('updated')
-      })
     });
+      
+    codeEditor.editor().on('change', function(change) {
+      console.log('changed',change)
+    })
+    codeEditor.editor().on('update', function() {
+      console.log('updated')
+    })
+  });
 
-    //need to finish importing all of the sublime shortcuts and whatnot: http://codemirror.net/doc/manual.html#addons
+  //need to finish importing all of the sublime shortcuts and whatnot: http://codemirror.net/doc/manual.html#addons
 
-    // this is the syntax needed for .getValue and .setValue.  rightOriginal, leftOriginal, and editor are all
-    // of the possible CodeMirror instances; we only use editor and rightOriginal in our version right now.
-    // console.log('editor: ',codeEditor.editor().getValue(),"\n",'original: ',codeEditor.rightOriginal().getValue())
-    // codeEditor.editor().setValue('this is a test')
+  // this is the syntax needed for .getValue and .setValue.  rightOriginal, leftOriginal, and editor are all
+  // of the possible CodeMirror instances; we only use editor and rightOriginal in our version right now.
+  // console.log('editor: ',codeEditor.editor().getValue(),"\n",'original: ',codeEditor.rightOriginal().getValue())
+  // codeEditor.editor().setValue('this is a test')
 
-    return codeEditor
-  }
+  // return codeEditor
+}
 
 
 
