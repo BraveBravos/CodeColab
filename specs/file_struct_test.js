ab586869e7ad56aeeb12f7b7c985b70d1faf3122
SFoskitt <stephanie.foskitt@gmail.com>
2015-05-09 16:36:59 -0500

    add fileStruct.js specs for testing

---
+++ specs/file_struct_test.js
@@ -0,0 +1,17 @@
+'use strict';
+//var app = require('../client/app/main/fileStruct.js');
+
+describe('CodeColab tests for client/app/main/fileStruct.js',function(){
+  
+  describe('fileStruct.js methods',function(){
+
+    it('getTree is defined',function(){
+      expect('getTree').toBeDefined();
+    });
+
+    // it('app listen is defined',function(){
+    //   expect('app.listen').toBeDefined();
+    // });
+
+  }); 
+});
