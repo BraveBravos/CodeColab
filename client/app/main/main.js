e34b80bbce82e36da52f17190123e2d2e39aefa4
Enja Reyes <enja.reyes@gmail.com>
2015-05-09 14:07:18 -0500

    clearing out box

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -40,7 +40,7 @@ angular.module('codeColab.main', [])
 
   $scope.commit = function(message){
     Share.commit(message)
-    var input = document.getElementById('commitMessage');
+    var input = document.getElementById('commitMessageg');
     input.value=''
   }
 
