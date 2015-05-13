1f674f80f9cf00f9caae5ced2ff9d4f98fc105f9
SFoskitt <stephanie.foskitt@gmail.com>
2015-05-13 15:55:36 -0500

    tests are running - now with Nyan and Unicorn

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,78 @@
 module.exports = function(config){
   config.set({
 
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath : './',
 
-    files : [
-      'specs/*.js',
-    ],
-
-    autoWatch : true,
-
-    singleRun : true,
-
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    browsers : ['PhantomJS'],
+    // list of files / patterns to load in the browser
+    files: [
+      // angular source
+      'client/bower_components/angular/angular.min.js',
+      'client/bower_components/angular-route/angular-route.min.js',
+      'client/bower_components/angular-mocks/angular-mocks.js',
+      'client/bower_components/jquery/dist/jquery.min.js',
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter',
-            'karma-phantomjs-launcher'
-            ],
+      // our app files
+      'client/app/app.js',
+      'client/app/services.js',
+      'client/app/main/*.js',
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+      // spec files
+      'specs/*.js'
+    ],
 
+    // list of files to exclude
+    exclude: [
+        'karma.conf.js'
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+
+    },
+
+    // reporters: ['progress'],
+
+    reporters: ['progress', 'nyan', 'unicorn'],
+    nyanReporter: { suppressErrorReport: false }, 
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch : false,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers : ['Chrome'],
+    // browsers : ['PhantomJS'],
+
+    singleRun : true
+
+    // junitReporter : {
+    //   outputFile: 'test_out/unit.xml',
+    //   suite: 'unit'
+    // }
+
+    // plugins : [
+    //         'karma-chrome-launcher',
+    //         'karma-firefox-launcher',
+    //         'karma-jasmine',
+    //         'karma-junit-reporter',
+    //         'karma-phantomjs-launcher'
+    //         ],
+            
   });
 };
---
+++ specs/fileTree_test.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+// // var struct = require('../client/app/main/fileStruct.js');
+
+// describe('CodeColab tests for client/app/main/fileStruct.js',function(){
+  
+//   describe('fileStruct.js methods',function(){
+
+//     it('getTree is defined',function(){
+//       expect('getTree').toBeDefined();
+//     });
+
+//     // it('app listen is defined',function(){
+//     //   expect('app.listen').toBeDefined();
+//     // });
+
+//   }); 
+// });
