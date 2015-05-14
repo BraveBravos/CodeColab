0cafc61b546f8415e16b53900201fbace50ec425
Pat Dalberg <pat.dalberg@gmail.com>
2015-05-14 16:24:26 -0500

    more tests and some refactoring to use angular style in template and controller

--- specs/videochat_test.js
+++ specs/videochat_test.js
@@ -18,6 +18,26 @@ describe('videochat tests',function(){
       expect('connection.onspeaking').toBeDefined();
     });
 
+    it('connection.onsilence is defined',function(){
+      expect('connection.onsilence').toBeDefined();
+    });
+
+    it('connection.onunmute is defined',function(){
+      expect('connection.onunmute').toBeDefined();
+    });
+
+  });
+
+  describe('join and leave chat methods are defined',function(){
+
+    it('joinChat method is defined',function(){
+      expect('joinChat').toBeDefined();
+    });
+
+    it('leaveChat method is defined',function(){
+      expect('leaveChat').toBeDefined();
+    });
+
   });
 
 });
