5ec7ad2288d1342e3c873688516c89f3c37b27eb
SFoskitt <stephanie.foskitt@gmail.com>
2015-05-18 18:58:52 -0500

    railroaded push

--- karma.conf.js
+++ karma.conf.js
@@ -68,7 +68,6 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
     'mocha'
-
     // 'unicorn', 
     // 'nyan' 
     ],
