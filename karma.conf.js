baf3fecd6b3ff7662ec74272ab6346053471732c
SFoskitt <stephanie.foskitt@gmail.com>
2015-05-18 18:44:10 -0500

    my build was overridden

--- karma.conf.js
+++ karma.conf.js
@@ -68,6 +68,7 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
     'mocha'
+
     // 'unicorn', 
     // 'nyan' 
     ],
