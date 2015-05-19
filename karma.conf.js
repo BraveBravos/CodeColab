e96c8a3bc2824db6209aa38b31036c0b5bb355f5
SFoskitt <stephanie.foskitt@gmail.com>
2015-05-18 19:00:26 -0500

    feel like giving up

--- karma.conf.js
+++ karma.conf.js
@@ -67,6 +67,7 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
+    
     'mocha'
     // 'unicorn', 
     // 'nyan' 
