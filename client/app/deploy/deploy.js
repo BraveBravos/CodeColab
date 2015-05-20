d168a6351a2219f27d244359252a414842e7cd14
Phil Dornfeld <phillydorn75@gmail.com>
2015-05-20 00:41:20 -0500

    fixes error when cancel is selected on deploy modal

--- client/app/deploy/deploy.js
+++ client/app/deploy/deploy.js
@@ -6,6 +6,13 @@ angular.module('codeColab.deploy', [])
   $scope.deploy = 'deploying';
   $scope.first = true;
   $scope.deploying = false;
+  $scope.dismissed = false;
+
+  $scope.$watch('dismissed', function(newval, oldval) {
+    if (newval!==oldval) {
+      $location.path('/');
+    }
+  })
 
   $scope.checkForApp = function() {
     Share.checkForApp($scope, localStorage.repo)
@@ -13,31 +20,31 @@ angular.module('codeColab.deploy', [])
 
 
   $scope.deployApp = function(){
-    var modalDismissed = false;
     var validName = false;
     var name;
       if ($scope.first) {
         bootbox.prompt("What would you like to name your app?", function (enterName) {
-          if (enterName === null) {
-            modalDismissed = true;
-            return;
-          } else {
-            name = enterName
-            $scope.first = false;
-            validName = Share.checkName(name)
-            if (validName) {
-              $scope.deploying = true;
-              Share.deployApp($scope, name);
+            if (enterName === null) {
+              $scope.$apply(function() {$scope.dismissed = true});
+              return;
             } else {
-              $scope.deployApp();
+              name = enterName
+              $scope.first = false;
+              validName = Share.checkName(name)
+              if (validName) {
+                $scope.deploying = true;
+                Share.deployApp($scope, name);
+              } else {
+                $scope.deployApp();
+              }
             }
-          }
        })
       } else {
         bootbox.prompt("Valid Heroku app names must start with a letter and can only contain lowercase letters, numbers,\
          and dashes. Please enter a valid name.", function (enterName) {
           if (enterName === null) {
-            $location.path('/');
+            $scope.$apply(function() {$scope.dismissed = true});
+              return;
           } else {
             name = enterName;
             validName = Share.checkName(name)
