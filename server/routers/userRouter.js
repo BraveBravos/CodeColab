c7b76106ee75fb64648f172139b6278729e76c7e
Phil Dornfeld <phillydorn75@gmail.com>
2015-05-22 15:10:58 -0500

    deploys app

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,6 @@ var user = require('../models/users.js');
 module.exports = function (app) {
 
   // app.get('/github', user.githubAuth);
-  app.get('/heroku', user.herokuAuth);
+  // app.get('/heroku', user.herokuAuth);
   app.get('/check', user.checkAuth);
 };
