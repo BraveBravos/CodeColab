9c9e33c319b967d47702ba2c90c69ffde75b58f7
Phil Dornfeld <phillydorn75@gmail.com>
2015-05-23 13:21:03 -0500

    moves logout route to auth

--- server/server.js
+++ server/server.js
@@ -15,12 +15,6 @@ app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() { console.log('Node app running on port', app.get('port')) });
 
 
-app.get('/logout', function (req, res){
-  req.logout();
-  res.redirect('/');
-})
-
-
 
 exports =module.exports=app;
 
