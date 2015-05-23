813264f57d7c9e4128e0accf94319cdf7e9f5c27
Phil Dornfeld <phillydorn75@gmail.com>
2015-05-23 12:36:41 -0500

    removes mongo databasename

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+if (!process.env.CLIENT_ID) var keys = require('../keys.js');
+
 var express = require('express');
     app = express(),
     server  = require('http').createServer(app),
     browserChannel = require('browserchannel').server,
     mongo = require('mongodb'),
     monk =require ('monk'),
-    db = monk('mongodb://heroku_app36344810:slkuae58qandst6sk9r58r57bl@ds031812.mongolab.com:31812/heroku_app36344810'),
+    db = monk(process.env.MONGOLAB_URI || keys.dbAddress),
     bodyParser = require ('body-parser'),
     Duplex = require( 'stream' ).Duplex,
     livedb = require( 'livedb' ),
     sharejs = require( 'share' ),
     liveDBMongoClient = require('livedb-mongo'),
-    dbClient =liveDBMongoClient('mongodb://heroku_app36344810:slkuae58qandst6sk9r58r57bl@ds031812.mongolab.com:31812/heroku_app36344810',
+    dbClient =liveDBMongoClient(process.env.MONGOLAB_URI || keys.dbAddress,
       {safe: true}),
     backend = livedb.client(dbClient),
     share = sharejs.server.createClient({
@@ -22,7 +24,6 @@ require('./auth')(app);
 require('./routes')(app);
 app.use('browserChannel', require('./models/browserChannel.js'));
 
-if (!process.env.CLIENT_ID) var keys = require('../keys.js');
 
 
 app.set('port', (process.env.PORT || 3000));
