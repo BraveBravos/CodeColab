5ec804671bd419dffb0f603993b475031e0d90f8
Phil Dornfeld <phillydorn75@gmail.com>
2015-05-23 13:18:37 -0500

    moves browserchannel out of server

--- server/models/browserChannel.js
+++ server/models/browserChannel.js
@@ -1,5 +1,26 @@
-module.exports = function(client) {
+if (!process.env.CLIENT_ID) var keys = require('../../keys.js');
 
+
+var browserChannel = require('browserchannel').server,
+    mongo = require('mongodb'),
+    monk =require ('monk'),
+    db = monk(process.env.MONGOLAB_URI || keys.dbAddress),
+    Duplex = require( 'stream' ).Duplex,
+    sharejs = require( 'share' ),
+    livedb = require( 'livedb' )
+    liveDBMongoClient = require('livedb-mongo'),
+    dbClient =liveDBMongoClient(process.env.MONGOLAB_URI || keys.dbAddress,
+      {safe: true}),
+    backend = livedb.client(dbClient),
+    share = sharejs.server.createClient({
+      backend: backend
+    });
+
+
+
+module.exports = function(app) {
+
+   app.use(browserChannel(function(client) {
     var stream = new Duplex({objectMode: true});
 
     stream._read = function() {};
@@ -24,4 +45,5 @@ module.exports = function(client) {
     });
 
     return share.listen(stream);
+  }));
 }
--- server/server.js
+++ server/server.js
@@ -1,28 +1,13 @@
-if (!process.env.CLIENT_ID) var keys = require('../keys.js');
 
 var express = require('express');
     app = express(),
     server  = require('http').createServer(app),
-    browserChannel = require('browserchannel').server,
-    mongo = require('mongodb'),
-    monk =require ('monk'),
-    db = monk(process.env.MONGOLAB_URI || keys.dbAddress),
-    bodyParser = require ('body-parser'),
-    Duplex = require( 'stream' ).Duplex,
-    livedb = require( 'livedb' ),
-    sharejs = require( 'share' ),
-    liveDBMongoClient = require('livedb-mongo'),
-    dbClient =liveDBMongoClient(process.env.MONGOLAB_URI || keys.dbAddress,
-      {safe: true}),
-    backend = livedb.client(dbClient),
-    share = sharejs.server.createClient({
-      backend: backend
-    });
+    bodyParser = require ('body-parser');
 
 require('./config/express')(app);
 require('./auth')(app);
 require('./routes')(app);
-app.use('browserChannel', require('./models/browserChannel.js'));
+require('./models/browserChannel')(app);
 
 
 
@@ -34,33 +19,6 @@ app.get('/logout', function (req, res){
   req.logout();
   res.redirect('/');
 })
-
-app.use(browserChannel(function(client) {
-    var stream = new Duplex({objectMode: true});
-
-    stream._read = function() {};
-    stream._write = function(chunk, encoding, callback) {
-      if (client.state !== 'closed') {
-        client.send(chunk);
-      }
-      callback();
-    };
-
-    client.on('message', function(data) {
-      stream.push(data);
-    });
-
-    client.on('close', function(reason) {
-      stream.push(null);
-      stream.emit('close');
-    });
-
-    stream.on('end', function() {
-      client.close();
-    });
-
-    return share.listen(stream);
-  }));
 
 
 
